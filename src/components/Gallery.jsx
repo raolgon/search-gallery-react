316f8fbbad38adeff97dba96a6caa5e976743821
Rafael Olivares <rafagonzal67@gmail.com>
2021-10-05 00:24:54 -0500

    Basic search and display components made, also delete some files

---
+++ src/components/Gallery.jsx
@@ -0,0 +1,10 @@
+const Gallery = (props) => {
+    return (
+        <div className="box columns">
+            <h1 className="h1">Gallery</h1>
+        </div>
+    )
+
+}
+
+export default Gallery;
---
+++ src/components/ImageSearch.jsx
@@ -0,0 +1,10 @@
+const SearchInput = (props) => {
+    return (
+        <div className="box">
+            <input className="input" type="text" placeholder="Type your search"></input>
+            <button className="button">Search</button>
+        </div>
+    )
+}
+
+export default SearchInput;
